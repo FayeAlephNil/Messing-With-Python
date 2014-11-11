0ec51f595fd61a4503cf9f93bf2d8c52b40b17b8
Strikingwolf <Strikingwolf2012@gmail.com>
2014-11-11 14:29:56 -0600

    Add an all option for primes and do some bug fixing

--- src/maths/primes.py
+++ src/maths/primes.py
@@ -7,7 +7,7 @@ def check(n, primes):
 def prime(n):
     a = 2
     primes = []
-    if n > 2:
+    if n > 0:
         primes = [2]
         while len(primes) < n:
             a = a + 1
@@ -16,31 +16,50 @@ def prime(n):
     return primes
 def printPrimesEasily(n):
     a = 2
-    if n > 2:
+    if n > 0:
+        print("2")
         primes = [2]
         while len(primes) < n:
             a = a + 1
             if check(a, primes):
                 primes.append(a)
                 print(a)
+
+def all():
+    primes = [2]
+    a = 2
+    while 1:
+        a = a+1
+        if check(a, primes):
+            primes.append(a)
+            print(a)
 def run():
-    number_wanted = int(input("Number of Primes (must be positive and not equal to 0)"))
-    way = input("Do you want to print primes successively or all at once (s or all)?")
-    if number_wanted <= 0:
-        print("Number of Primes must be positive and not equal to 0")
-        yes_or_no = input("Try again? (yes or no) \n")
-        if yes_or_no == "yes":
-            run()
-    else:
-        if way == "all":
-            primes = prime(number_wanted)
-            for num in primes:
-                print(num)
-        elif way == "s":
-            printPrimesEasily(number_wanted)
-        else:
-            print("Try s or all next time")
+    all_or_some = input("Do you want to print all the primes or only a certain number?(all or some)")
+    if all_or_some == "some":
+        number_wanted = int(input("Number of Primes (must be positive and not equal to 0)"))
+        way = input("Do you want to print primes successively or all at once (s or all)?")
+        if number_wanted <= 0:
+            print("Number of Primes must be positive and not equal to 0")
             yes_or_no = input("Try again? (yes or no) \n")
             if yes_or_no == "yes":
                 run()
+        else:
+            if way == "all":
+                primes = prime(number_wanted)
+                for num in primes:
+                    print(num)
+            elif way == "s":
+                printPrimesEasily(number_wanted)
+            else:
+                print("Try s or all next time")
+                yes_or_no = input("Try again? (yes or no) \n")
+                if yes_or_no == "yes":
+                    run()
+    elif all_or_some == "all":
+        all()
+    else:
+        print("You can only enter all or some!")
+        yes_or_no = input("Try again? (yes or no) \n")
+        if yes_or_no == "yes":
+            run()
 run()
