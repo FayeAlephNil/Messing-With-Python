a019d4900ea0e035671fd755dcd34d103abc939b
Strikingwolf <Strikingwolf2012@gmail.com>
2014-11-11 13:10:16 -0600

    Add different ways to print the primes

--- src/maths/primes.py
+++ src/maths/primes.py
@@ -14,16 +14,33 @@ def prime(n):
             if check(a, primes):
                 primes.append(a)
     return primes
+def printPrimesEasily(n):
+    a = 2
+    if n > 2:
+        primes = [2]
+        while len(primes) < n:
+            a = a + 1
+            if check(a, primes):
+                primes.append(a)
+                print(a)
 def run():
     number_wanted = int(input("Number of Primes (must be positive and not equal to 0)"))
-    primes = prime(number_wanted)
+    way = input("Do you want to print primes successively or all at once (s or all)?")
     if number_wanted <= 0:
         print("Number of Primes must be positive and not equal to 0")
         yes_or_no = input("Try again? (yes or no) \n")
         if yes_or_no == "yes":
             run()
     else:
-        primes = prime(number_wanted)
-        for num in primes:
-            print(num)
+        if way == "all":
+            primes = prime(number_wanted)
+            for num in primes:
+                print(num)
+        elif way == "s":
+            printPrimesEasily(number_wanted)
+        else:
+            print("Try s or all next time")
+            yes_or_no = input("Try again? (yes or no) \n")
+            if yes_or_no == "yes":
+                run()
 run()
