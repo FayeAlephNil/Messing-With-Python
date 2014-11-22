b21b49346266daa21c09da2d97321afcefdced88
Strikingwolf <Strikingwolf2012@gmail.com>
2014-11-21 18:27:28 -0600

    finishing dragon_curve.py

--- src/maths/dragon_curve.py
+++ src/maths/dragon_curve.py
@@ -1,4 +1,3 @@
-
 def convert(char):
     if char == 'D':
         return "DL"
@@ -14,20 +13,20 @@ def get_stage(stage):
     output = ""
     if stage == 1:
         return "D"
-    elif stage > 1:
-        output = get_stage(stage-1)
-        for char in output:
-            output + convert(char)
-    elif stage <= 0:
-        print("Stage must be greater than 0")
-        yes_no = input("Do you want to go again?")
-        if yes_no == "yea":
+    elif 1 < stage < 998:
+        last = get_stage(stage-1)
+        for char in last:
+            output += (convert(char))
+    elif stage <= 0 or stage > 997:
+        print("Stage must be greater than 0 and less than 998")
+        yes_no = input("Do you want to go again? (yes or no)\n")
+        if yes_no == "yes":
             run()
     return output
 
 
 def run():
-    stage = int(input("What stage of the dragon curve do you (must be greater than 0)\n"))
+    stage = int(input("What stage of the dragon curve do you (must be greater than 0 and less than 998)\n"))
     print(get_stage(stage))
 
 if __name__ == "__main__":
